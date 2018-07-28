a88ce687e097b04bf502ea1937f0cc26a115ef13
zytris <41842093+zytris@users.noreply.github.com>
2018-07-28 19:36:11 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,7 +145,7 @@
 						$message._hide();
 
 					// Disable submit.
-						$submit.disabled = true;
+						//$submit.disabled = true;
 
 					// Process form.
 					// Note: Doesn't actually do anything yet (other than report back with a "thank you"),
